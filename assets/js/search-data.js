0eab1a53b2ae24af1952ffc76b5ffdcfe09244a3
Anika-Tahsin-S <71835381+Anika-Tahsin-S@users.noreply.github.com>
2025-08-22 21:01:50 +0000

    Deploying to gh-pages from @ Anika-Tahsin-S/Anika-Tahsin-S.github.io@9936a24b579753483215f3d436c5fd24e1b357e5 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -39,7 +39,7 @@ ninja.data = [{
           },
         },{id: "nav-teaching",
           title: "teaching",
-          description: "Materials for courses you taught. Replace this text with your description.",
+          description: "Workshops, tutoring, and thesis mentoring",
           section: "Navigation",
           handler: () => {
             window.location.href = "/teaching/";
